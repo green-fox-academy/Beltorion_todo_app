88b1db942bf9ec321a510a2f07176a8d2b4b2afd
Viktor Gazsi <Beltorion@users.noreply.github.com>
2020-12-14 06:19:59 +0100

    todo app started

---
+++ src/com/beltorion/Main.java
@@ -0,0 +1,54 @@
+package com.beltorion;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length == 0) {
+            userManual();
+        } else if ((args[0].equals("-l"))) {
+            listTasks();
+        } else if ((args[0].equals("-a"))) {
+            System.out.println("New task");
+        } else if ((args[0].equals("-r"))) {
+            System.out.println("Remove task");
+        } else if ((args[0].equals("-c"))) {
+            System.out.println("complete task");
+        } else {
+            System.out.println("not valid argument");
+        }
+    }
+
+    private static void listTasks() throws IOException {
+        try {
+            Path filePath = Paths.get("src/todoapp/todolist");
+            List<String> lines = Files.readAllLines(filePath);
+            int lineNumber = 1;
+            for (String line : lines) {
+                System.out.println(lineNumber + ". " + line);
+                lineNumber++;
+            }
+        } catch (IOException e) {
+            System.out.println("Unable to read file: todolist.txt");
+        }
+
+    }
+
+    private static void userManual() {
+        System.out.println("\nParancssori Todo applikáció\n" +
+                "=============================\n" +
+                "\n" +
+                "Parancssori argumentumok:\n" +
+                "    -l   Kilistázza a feladatokat\n" +
+                "    -a   Új feladatot ad hozzá\n" +
+                "    -r   Eltávolít egy feladatot\n" +
+                "    -c   Teljesít egy feladatot.");
+    }
+
+}
---
+++ src/com/beltorion/ToDoAppTasks.java
@@ -0,0 +1,4 @@
+package com.beltorion;
+
+public class ToDoAppTasks {
+}
