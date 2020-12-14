9591841fd716eea8d6caff5244b37306b1a4cc69
Viktor Gazsi <Beltorion@users.noreply.github.com>
2020-12-14 07:08:50 +0100

    Task has been moved to ToDoAppTasks class

--- src/com/beltorion/Main.java
+++ src/com/beltorion/Main.java
@@ -11,9 +11,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         if (args.length == 0) {
-            userManual();
+            ToDoAppTasks.userManual();
         } else if ((args[0].equals("-l"))) {
-            listTasks();
+            ToDoAppTasks.listTasks();
         } else if ((args[0].equals("-a"))) {
             System.out.println("New task");
         } else if ((args[0].equals("-r"))) {
@@ -25,33 +25,10 @@ public class Main {
         }
     }
 
-    private static void listTasks() throws IOException {
-        try {
-            Path filePath = Paths.get("src/com/beltorion/todolist");
-            List<String> lines = Files.readAllLines(filePath);
-            if (lines.isEmpty()) {
-                System.out.println("Nincs mára tennivalód! :)");
-            }else {
-                int lineNumber = 1;
-                for (String line : lines) {
-                    System.out.println(lineNumber + ". " + line);
-                    lineNumber++;
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("Unable to read file: todolist.txt");
-        }
-    }
 
-    private static void userManual() {
-        System.out.println("\nParancssori Todo applikáció\n" +
-                "=============================\n" +
-                "\n" +
-                "Parancssori argumentumok:\n" +
-                "    -l   Kilistázza a feladatokat\n" +
-                "    -a   Új feladatot ad hozzá\n" +
-                "    -r   Eltávolít egy feladatot\n" +
-                "    -c   Teljesít egy feladatot.");
+    private static void addTask(){
+
+
     }
 
 }
--- src/com/beltorion/ToDoAppTasks.java
+++ src/com/beltorion/ToDoAppTasks.java
@@ -1,4 +1,40 @@
 package com.beltorion;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class ToDoAppTasks {
+
+    static void listTasks() throws IOException {
+        try {
+            Path filePath = Paths.get("src/com/beltorion/todolist");
+            List<String> lines = Files.readAllLines(filePath);
+            if (lines.isEmpty()) {
+                System.out.println("Nincs mára tennivalód! :)");
+            }else {
+                int lineNumber = 1;
+                for (String line : lines) {
+                    System.out.println(lineNumber + ". " + line);
+                    lineNumber++;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Unable to read file: todolist.txt");
+        }
+    }
+
+    static void userManual() {
+        System.out.println("\nParancssori Todo applikáció\n" +
+                "=============================\n" +
+                "\n" +
+                "Parancssori argumentumok:\n" +
+                "    -l   Kilistázza a feladatokat\n" +
+                "    -a   Új feladatot ad hozzá\n" +
+                "    -r   Eltávolít egy feladatot\n" +
+                "    -c   Teljesít egy feladatot.");
+    }
+
 }
